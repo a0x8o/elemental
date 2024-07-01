8fad32c5b2fe2927b5c48f46a05ece974236e6f1
Alex Taylor-Barreto <alex.barreto@databricks.com>
2024-07-01 17:35:11 -0500

    Add an EnsureComm call to make sure things are sane (#182)

--- include/El/blas_like/level1/Copy/TranslateBetweenGrids.hpp
+++ include/El/blas_like/level1/Copy/TranslateBetweenGrids.hpp
@@ -3891,6 +3891,10 @@ void TranslateBetweenGrids(
   Int ALDim = A.LDim();
 
   mpi::Comm const& viewingCommB = B.Grid().ViewingComm();
+<<<<<<< HEAD
+=======
+
+>>>>>>> fe19d9fcd (Add an EnsureComm call to make sure things are sane (#182))
   bool const inAGrid = A.Participating();
   bool const inBGrid = B.Participating();
 
@@ -3950,6 +3954,13 @@ void TranslateBetweenGrids(
       (maybeMultiSync.has_value()
        ? *maybeMultiSync
        : (inAGrid ? syncInfoA : syncInfoB));
+<<<<<<< HEAD
+=======
+
+  // Collective!
+  mpi::EnsureComm<T, Collective::SEND>(viewingCommB, syncInfo);
+  mpi::EnsureComm<T, Collective::RECV>(viewingCommB, syncInfo);
+>>>>>>> fe19d9fcd (Add an EnsureComm call to make sure things are sane (#182))
 
   // Translate the ranks from A's VC communicator to B's viewing so
   // that we can match send/recv communicators. Since A's VC
