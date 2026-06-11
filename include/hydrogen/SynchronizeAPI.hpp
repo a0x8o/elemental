7155223f22efeb114c47d9337a6c61d5a46cdf66
Alex Barreto <axbo@lavabit.com>
2026-06-11 14:25:08 -0400

    Reduce the number of event record calls (#153)
    * Reduce the number of event recordings when unnecessary to call
    
    * Apply suggestions from code review
    
    Co-authored-by: Tom Benson <benson31@llnl.gov>
    
    * Formatting
    
    ---------
    
    Co-authored-by: Tom Benson <benson31@llnl.gov>

--- include/hydrogen/SynchronizeAPI.hpp
+++ include/hydrogen/SynchronizeAPI.hpp
@@ -20,17 +20,47 @@ void AddSynchronizationPoint(SyncInfo<D> const &master,
         // synchronization points. Skip "other" call recursively with the rest.
         if (master.Stream() == other.Stream())
         {
+<<<<<<< HEAD
             if constexpr (sizeof...(others) > 0UL)
                 AddSynchronizationPoint(master, others...);
+=======
+            AddSynchronizationPoint(master, others...);
+>>>>>>> 315bc6461 (Reduce the number of event record calls (#153))
             return;
         }
     }
 #endif // HYDROGEN_HAVE_GPU
 
     AddSynchronizationPoint(master);
+<<<<<<< HEAD
     int dummy[] = {(details::AddSyncPoint(master, other), 0),
                    (details::AddSyncPoint(master, others), 0)...};
     (void)dummy;
+=======
+    int dummy[] = {(details::AddSyncPoint(master, b),
+                    details::AddSyncPoint(master, others), 0)...};
+    (void)dummy;
+}
+
+// Specialization of the above function for two arguments
+template <Device D, Device D2>
+void AddSynchronizationPoint(SyncInfo<D> const &master,
+                             SyncInfo<D2> const &other)
+{
+#ifdef HYDROGEN_HAVE_GPU
+    if constexpr (D == Device::GPU && D == D2)
+    {
+        // When the two streams are the same, there is no need to create
+        // synchronization points.
+        if (master.Stream() == other.Stream())
+        {
+            return;
+        }
+    }
+#endif // HYDROGEN_HAVE_GPU
+
+    AddSynchronizationPoint(master);
+>>>>>>> 315bc6461 (Reduce the number of event record calls (#153))
 }
 
 template <Device D, Device... Ds>
